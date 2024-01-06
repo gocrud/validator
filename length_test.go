50138f7a4830716802931e96ac87e388cb454600
0w3w0 <41020733@qq.com>
2024-01-06 17:50:35 +0800

    🧪 test: Length

--- length_test.go
+++ length_test.go
@@ -1,12 +1,17 @@
 package validator
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestLength(t *testing.T) {
-	var s = 3
+	var s = ""
 	err := Validate(s, Optional(Length(3, 0)))
 	require.NoError(t, err)
+
+	s = "12"
+	err = Validate(s, Optional(Length(3, 0)))
+	require.Error(t, err)
 }
