8d2791bba9add4ad59d86594e7dbcdd7657133e8
0w3w0 <41020733@qq.com>
2024-01-06 17:54:05 +0800

    🧪 test: range

--- range_test.go
+++ range_test.go
@@ -1,11 +1,17 @@
 package validator
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestIntRange(t *testing.T) {
 	var i = 0
 	err := Validate(i, Optional(IntRange(3, 0).Msg("最小值为3")))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
+
+	i = 2
+	err = Validate(i, Optional(IntRange(3, 6).Msg("取值范围为3-6")))
+	require.Error(t, err)
 }
